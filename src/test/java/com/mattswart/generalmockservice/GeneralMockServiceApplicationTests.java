a5ad5c028a423d4b2ca30ded8f413abd35f1da8f
Matt Swart <matt.swart@cba.com.au>
2025-07-02 11:25:40 +1000

    initial

---
+++ src/test/java/com/mattswart/generalmockservice/GeneralMockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mattswart.generalmockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralMockServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
